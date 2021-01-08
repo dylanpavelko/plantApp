3a2781c953b6af4a25395426ea07a4be6c94adab
Dylan Pavelko <dylanpavelko@users.noreply.github.com>
2021-01-08 11:31:26 -0800

    make bbch optional

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,4 @@
 class Species < ApplicationRecord
     belongs_to :genus, :class_name => "Genu", :foreign_key => "genus_id"
-    belongs_to :bbch_profile, :class_name => "BbchProfile", :foreign_key => "bbch_profile_id"
+    belongs_to :bbch_profile, :class_name => "BbchProfile", :foreign_key => "bbch_profile_id", :optional => true
 end
