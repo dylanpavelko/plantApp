edcf7e7d23cd68897d1cdea8f242988f886805bd
Dylan Pavelko <dylanpavelko@users.noreply.github.com>
2022-02-18 13:58:11 -0800

    improved migration

--- db/migrate/20220218210901_add_inactive_to_plant_instance.rb
+++ db/migrate/20220218210901_add_inactive_to_plant_instance.rb
@@ -1,5 +1,5 @@
 class AddInactiveToPlantInstance < ActiveRecord::Migration[5.2]
   def change
-    add_column :plant_instances, :inactive, :boolean
+    add_column :plant_instances, :inactive, :boolean,  null: false, default: false
   end
 end
