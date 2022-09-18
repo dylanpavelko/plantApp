8fce80ba380251621b13f86799407b4095b67f0e
Dylan Pavelko <dylanpavelko@users.noreply.github.com>
2022-09-17 23:24:44 -0700

    trying to fix s3 images

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
     get '/signup', to: 'users#new', as: 'signup'
     get '/login', to: 'sessions#new', as: 'login'
     get '/logout', to: 'sessions#destroy', as: 'logout'
+
     
     root 'home#index'
 end
